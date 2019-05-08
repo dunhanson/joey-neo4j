d873477efd28ddd0f22137a68e2f959448477cc1
dunhanson <dunhanson@gmail.com>
2019-05-08 10:55:40 +0800

    Neo4jUtils runAfterClose test

--- src/test/java/cn/joey/neo4j/test/StartTest.java
+++ src/test/java/cn/joey/neo4j/test/StartTest.java
@@ -5,8 +5,8 @@ import cn.joey.neo4j.utils.Neo4jUtils;
 
 public class StartTest {
     public static void main(String[] args) {
-        String cypher = "";
-        Neo4jUtils.run(cypher, Project.class).forEach(project -> {
+        String cypher = "MATCH (n:Project) RETURN n LIMIT 25";
+        Neo4jUtils.runAfterClose(cypher, Project.class).forEach(project -> {
             System.out.println(project);
         });
     }
